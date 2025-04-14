caf827ce7c2a3dac114a3ee1fc8557ab921e1ff0
xdsun777 <charmgo@qq.com>
2025-04-15 06:13:11 +0800

    init

---
+++ src/app.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="app">
+        <button @click="count++">{{ count }}</button>
+        <img src="@assets/navbar-logo-dark.png" alt="rspack logo" width="200" height="200" />
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            count: 0
+        }
+    }
+}
+</script>
+
+<style></style>
